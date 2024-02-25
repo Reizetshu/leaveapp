f052d33956ff974d3ee13a8b821316b67fcd587b
markbensonhernando <mbhernando27@gmail.com>
2024-02-25 14:22:15 +0800

    Feature: Adding Login and Registration Form with tailwindcss.

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,16 +1,17 @@
 import Link from 'next/link';
 
-const LoginForm = () => {
+const RegisterForm = () => {
   return (
     <div className='grid place-items-center h-screen'>
       <div className='shadow-lg p-5 rounded-lg border-t-4 border-green-800'>
         <h1 className=' text-center text-xl font-bold my-4'>
           Leave Management System
           <br />
-          Login
+          Register
         </h1>
 
         <form className='flex flex-col gap-3'>
+          <input className='inputLogin' type='text' placeholder='Full Name' />
           <input className='inputLogin' type='text' placeholder='Email' />
           <input
             className='inputLogin'
@@ -18,15 +19,15 @@ const LoginForm = () => {
             placeholder='Password'
           />
           <button className='bg-green-600 text-white fon-bold cursor-pointer px=6 py-2 rounded'>
-            Login
+            Register
           </button>
 
           <div className='bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2'>
             Error message
           </div>
 
-          <Link className='text-sm mt-3 text-right' href={'/register'}>
-            Don't have an account? <span className='underline'>Register</span>
+          <Link className='text-sm mt-3 text-right' href={'/'}>
+            Already have an account? <span className='underline'>Login</span>
           </Link>
         </form>
       </div>
@@ -34,4 +35,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default RegisterForm;
